47f215bf6ad83cf9f587628626a268e8130e9108
thbdias <thiagobalbino91@gmail.com>
2016-02-16 19:07:34 -0200

    criado arquivo symbTab.c e symbTab.h

---
+++ symbTab.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+static char tabelaSimbolo [500][256]; //500 registros  ---  cada registro possui um tamanho de 255 char
+static int codToken [500]; //armazena o codigo do token da kw/id correspondente na tabelaSimbolo
+
+void inicialize (){
+	
+}//end inicialize
