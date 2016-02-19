b34f2a8a1240c93aa0b91998faafb4f3c29dd141
thbdias <thiagobalbino91@gmail.com>
2016-02-19 19:35:19 -0200

    estrutura alterada

--- LexAnalyzer.cpp
+++ LexAnalyzer.cpp
@@ -4,3 +4,37 @@
 	Luiz Gustavo Avelino Mendes
 	Thiago Henrique Balbino Dias
 */
+
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include "SymbTab.h"
+
+class LexAnalyzer{
+	private:
+		void teste1(){
+			SymbTab tabela;
+
+			tabela.inicialize();
+			tabela.exibir();
+		}//end teste1
+
+
+
+	public:
+		LexAnalyzer(){} //construtor
+		~LexAnalyzer(){} //destrutor
+
+		void controle (){
+			teste1();
+		}//end controle
+
+};
+
+int main(){
+	LexAnalyzer lex;
+
+	lex.controle();
+	
+	return 0;
+}
