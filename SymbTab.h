5e3f579ba6d4471cc54b346d1d45754f7d1f9a88
thbdias <thiagobalbino91@gmail.com>
2016-02-19 19:33:59 -0200

    arquivo SymbTab.h criado

---
+++ SymbTab.h
@@ -0,0 +1,30 @@
+/*
+	Membros do grupo:
+	Herbert Alves Batista
+	Luiz Gustavo Avelino Mendes
+	Thiago Henrique Balbino Dias
+*/
+
+#ifndef SYMBTAB_H
+	#define SYMBTAB_H
+
+	#include <stdio.h>
+	#include <iostream>
+	#include <string>
+	using namespace std;
+
+	class SymbTab{
+		private:
+			//o tamanho da matriz foi arbitrário
+			//necessário discussão do grupo	
+			//1-cod do identificador ---- 2-lexema
+			string tabSimb [500][2]; //tabela de simbolos
+
+		public:
+			SymbTab(); //construtor
+			~SymbTab(); //destrutor
+			void inicialize();
+			void exibir(); //teste
+	};
+
+#endif
