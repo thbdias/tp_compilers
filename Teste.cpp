858707d3ae7b680e3cdc75f928add6f51feab227
thbdias <thiagobalbino91@gmail.com>
2016-02-27 01:44:32 -0300

    testes

--- Teste.cpp
+++ Teste.cpp
@@ -21,10 +21,10 @@ class Teste{
 
 	//teste analisador lexico
 	//funcionando
-	//void teste2 (){
-	//	LexAnalyzer lex;
-	//	lex.testeLex();
-	//}
+	void teste2 (){
+		LexAnalyzer lex;
+		lex.testeLex();
+	}
 	
 	//teste analisador lexico
 	//void testeArq(){
@@ -34,20 +34,20 @@ class Teste{
 	//}
 	
 	//teste2 analisador lexico
-	void teste2al(){
-		LexAnalyzer lex;
+	//void teste2al(){
+		//LexAnalyzer lex;
 		//lex.testeLex();
-		lex.lerArq("exemplo.l");
+		//lex.lerArq("exemplo.l");
 		//lex.isAlfab('a');
-	}
+	//}
 };
 
 int main(int argc, char const *argv[])
 {
 	Teste teste;
 	//teste.teste1(); 
-	//teste.teste2();
+	teste.teste2();
 	//teste.testeArq();
-	teste.teste2al();
+	//teste.teste2al();
 	return 0;
 }
