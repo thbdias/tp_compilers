466c298f08ccfe3e1e98f3d1a88a64600a8a2a5d
thbdias <thiagobalbino91@gmail.com>
2016-03-06 03:03:15 -0300

    metodos que definem o alfabt

--- LexAnalyzer.cpp
+++ LexAnalyzer.cpp
@@ -86,3 +86,253 @@ void LexAnalyzer::setEstado (int est){
 int LexAnalyzer::getEstado (){
 	return ESTADO;
 }//end getEstado
+
+
+
+/*
+	metodo que verifica se char e' sinal igual (=)
+	@return: true or false
+ */
+bool LexAnalyzer::isIgual (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' abre parenteses '('
+	@return: true or false
+ */
+bool LexAnalyzer::isAParent (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' fecha parenteses ')'
+	@return: true or false
+ */
+bool LexAnalyzer::isFParent (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' sinal de maior (>)
+	@return: true or false
+ */
+bool LexAnalyzer::isMaior (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' sinal de menor (<)
+	@return: true or false
+ */
+bool LexAnalyzer::isMenor (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' virgula (,)
+	@return: true or false
+ */
+bool LexAnalyzer::isVirgula (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' sinal de mais (+)
+	@return: true or false
+ */
+bool LexAnalyzer::isMais (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' sinal de menos (-)
+	@return: true or false
+ */
+bool LexAnalyzer::isMenos (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' sinal de multiplicacao (*)
+	@return: true or false
+ */
+bool LexAnalyzer::isMult (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' sinal de divisao (/)
+	@return: true or false
+ */
+bool LexAnalyzer::isBarra (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' abre chaves ({)
+	@return: true or false
+ */
+bool LexAnalyzer::isACha (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' fecha chaves (})
+	@return: true or false
+ */
+bool LexAnalyzer::isFCha (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' abre aspas (")
+	@return: true or false
+ */
+bool LexAnalyzer::isAAspas (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' fecha aspas (")
+	@return: true or false
+ */
+bool LexAnalyzer::isFAspas (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' numero (0-9)
+	@return: true or false
+ */
+bool LexAnalyzer::isNumero (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' ponto e virgula (;)
+	@return: true or false
+ */
+bool LexAnalyzer::isSemicolon (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' letra minuscula (a-z)
+	@return: true or false
+ */
+bool LexAnalyzer::isLetraMi (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' letra maiuscula (A-Z)
+	@return: true or false
+ */
+bool LexAnalyzer::isLetraMa (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' sublinhado (_)
+	@return: true or false
+ */
+bool LexAnalyzer::isSublinhado (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' dois pontos (:)
+	@return: true or false
+ */
+bool LexAnalyzer::isDoisPonto (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' abre colchete ([)
+	@return: true or false
+ */
+bool LexAnalyzer::isAColc (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' fecha colchete (])
+	@return: true or false
+ */
+bool LexAnalyzer::isFColc (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' apostrofo (')
+	@return: true or false
+ */
+bool LexAnalyzer::isApost (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' sinal de exclamacao (!)
+	@return: true or false
+ */
+bool LexAnalyzer::isExclam (char ch){
+	return true;
+}//end isAtrib
+
+
+
+/*
+	metodo que verifica se char e' sinal de interrogacao (?)
+	@return: true or false
+ */
+bool LexAnalyzer::isQuestion (char ch){
+	return true;
+}//end isAtrib
