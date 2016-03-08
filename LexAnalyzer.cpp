175e0ccf5448cda5036dde111854e03b365a85d8
thbdias <thiagobalbino91@gmail.com>
2016-03-08 09:44:12 -0300

    Estrutura de dados alterada

--- LexAnalyzer.cpp
+++ LexAnalyzer.cpp
@@ -53,10 +53,10 @@ void LexAnalyzer::lerArq(const char * arq){
 	ifstream fin (arq); //abre arquivo para leitura
 	char ch; //char que sera lido do arquivo
 
-	if (isAlfbt('('))
+	if (isAlfbt('"'))
 		cout << "\n\nfuncionando\n\n";
-	else
-		cout << "\n\nNAO OK\n\n";
+	//else
+	//	cout << "\n\nNAO OK\n\n";
 
 	//while (fin.get(ch)){ //le um char do arquivo
 		//cout << ch; //imprimir na tela
@@ -73,56 +73,87 @@ void LexAnalyzer::lerArq(const char * arq){
 bool LexAnalyzer::isAlfbt (char ch){
 	bool resp = false;
 
-	if (isIgual(ch))
-		return true;
-	else if (isAParent(ch))
-			return true;
-	else if (isFParent(ch))
-			return true;
-	else if (isMaior(ch))
-			return true;
-	else if (isMenor(ch))
-			return true;
-	else if (isVirgula(ch))
-			return true;
-	else if (isMais(ch))
-			return true;
-	else if (isMenos(ch))
-			return true;
-	else if (isMult(ch))
-			return true;
-	else if (isBarra(ch))
-			return true;
-	else if (isACha(ch))
-			return true;
-	else if (isFCha(ch))
-			return true;
-	else if (isAAspas(ch))
-			return true;
-	else if (isFAspas(ch))
-			return true;
-	else if (isNumero(ch))
-			return true;
-	else if (isSemicolon(ch))
-			return true;
+	if (isNumero(ch)) //precisar alterar. q6, q8, q10, sao numeros
+		resp = true;
 	else if (isLetraMi(ch))
-			return true;
+			resp = true;
 	else if (isLetraMa(ch))
-			return true;
-	else if (isSublinhado(ch))
-			return true;
-	else if (isDoisPonto(ch))
-			return true;
-	else if (isAColc(ch))
-			return true;
-	else if (isFColc(ch))
-			return true;
-	else if (isApost(ch))
-			return true;
-	else if (isExclam(ch))
-			return true;
-	else if (isQuestion(ch))
-			return true;
+			resp = true;
+	else
+		switch (ch){
+			case '{':
+				ESTADO = 1;
+				resp = true;
+				break;
+			case '}':
+				ESTADO = 15;
+				resp = true;
+				break;
+			case '(':
+				ESTADO = 2;
+				resp = true;
+				break;
+			case ')':
+				ESTADO = 16;
+				resp = true;
+				break;
+			case ';':
+				ESTADO = 26;
+				resp = true;
+				break;
+			case ',':
+				ESTADO = 23;
+				resp = true;
+				break;
+			case '+':
+				ESTADO = 22;
+				resp = true;
+				break;
+			case '*':
+				ESTADO = 22;
+				resp = true;
+				break;
+			case '-':
+				ESTADO = 27;
+				resp = true;
+				break;
+			case '_':
+				resp = true;
+				break;
+			case ':':
+				resp = true;
+				break;
+			case '[':
+				resp = true;
+				break;
+			case ']':
+				resp = true;
+				break;
+			case '\'':		//apostrofo
+				resp = true;
+				break;
+			case '"':
+				resp = true;
+				break;
+			case '!':
+				resp = true;
+				break;
+			case '?':
+				resp = true;
+				break;	
+			case '>':
+				resp = true;
+				break;
+			case '<':
+				resp = true;
+				break;
+			case '=':
+				resp = true;
+				break;
+			case '/':
+				resp = true;
+				break;	
+		}//end switch
 
 	return resp;
 }//end isAlfbt
@@ -156,162 +187,12 @@ int LexAnalyzer::getEstado (){
 
 
 /*
-	metodo que verifica se char e' sinal igual (=)
-	@return: true or false
- */
-bool LexAnalyzer::isIgual (char ch){
-	return (ch == '=');
-}//end isIgual
-
-
-
-/*
-	metodo que verifica se char e' abre parenteses '('
-	@return: true or false
- */
-bool LexAnalyzer::isAParent (char ch){
-	return (ch == '(');
-}//end isAParent
-
-
-
-/*
-	metodo que verifica se char e' fecha parenteses ')'
-	@return: true or false
- */
-bool LexAnalyzer::isFParent (char ch){
-	return (ch == ')');
-}//end isFParent
-
-
-
-/*
-	metodo que verifica se char e' sinal de maior (>)
-	@return: true or false
- */
-bool LexAnalyzer::isMaior (char ch){
-	return (ch == '>');
-}//end isMaior
-
-
-
-/*
-	metodo que verifica se char e' sinal de menor (<)
-	@return: true or false
- */
-bool LexAnalyzer::isMenor (char ch){
-	return (ch == '<');
-}//end isMenor
-
-
-
-/*
-	metodo que verifica se char e' virgula (,)
-	@return: true or false
- */
-bool LexAnalyzer::isVirgula (char ch){
-	return (ch == ',');
-}//end isVirgula
-
-
-
-/*
-	metodo que verifica se char e' sinal de mais (+)
-	@return: true or false
- */
-bool LexAnalyzer::isMais (char ch){
-	return (ch == '+');
-}//end isMais
-
-
-
-/*
-	metodo que verifica se char e' sinal de menos (-)
-	@return: true or false
- */
-bool LexAnalyzer::isMenos (char ch){
-	return (ch == '-');
-}//end isMenos
-
-
-
-/*
-	metodo que verifica se char e' sinal de multiplicacao (*)
-	@return: true or false
- */
-bool LexAnalyzer::isMult (char ch){
-	return (ch == '*');
-}//end isMult
-
-
-
-/*
-	metodo que verifica se char e' sinal de divisao (/)
-	@return: true or false
- */
-bool LexAnalyzer::isBarra (char ch){
-	return (ch == '/');
-}//end isBarra
-
-
-
-/*
-	metodo que verifica se char e' abre chaves ({)
-	@return: true or false
- */
-bool LexAnalyzer::isACha (char ch){
-	return (ch == '{');
-}//end isACha
-
-
-
-/*
-	metodo que verifica se char e' fecha chaves (})
-	@return: true or false
- */
-bool LexAnalyzer::isFCha (char ch){
-	return (ch == '}');
-}//end isFCha
-
-
-
-/*
-	metodo que verifica se char e' abre aspas (")
-	@return: true or false
- */
-bool LexAnalyzer::isAAspas (char ch){
-	return (((int)ch == 34) || ((int)ch == 84) || ((int)ch == 93) || ((int)ch == 94));
-}//end isAAspas
-
-
-
-/*
-	metodo que verifica se char e' fecha aspas (")
-	@return: true or false
- */
-bool LexAnalyzer::isFAspas (char ch){
-	return (((int)ch == 34) || ((int)ch == 84) || ((int)ch == 93) || ((int)ch == 94));
-}//end isFSspas
-
-
-
-/*
 	metodo que verifica se char e' numero (0-9)
 	@return: true or false
  */
 bool LexAnalyzer::isNumero (char ch){
 	return (((int)ch >= 48) && ((int)ch <= 57));
 }//end isNumero
-
-
-
-/*
-	metodo que verifica se char e' ponto e virgula (;)
-	@return: true or false
- */
-bool LexAnalyzer::isSemicolon (char ch){
-	return (ch == ';');
-}//end isSemicolon
 
 
 
@@ -332,73 +213,3 @@ bool LexAnalyzer::isLetraMi (char ch){
 bool LexAnalyzer::isLetraMa (char ch){
 	return (((int)ch >= 65) && ((int)ch <= 90));
 }//end isLetraMa
-
-
-
-/*
-	metodo que verifica se char e' sublinhado (_)
-	@return: true or false
- */
-bool LexAnalyzer::isSublinhado (char ch){
-	return (ch == '_');
-}//end isSublinhado
-
-
-
-/*
-	metodo que verifica se char e' dois pontos (:)
-	@return: true or false
- */
-bool LexAnalyzer::isDoisPonto (char ch){
-	return (ch == ':');
-}//end isDoisPont
-
-
-
-/*
-	metodo que verifica se char e' abre colchete ([)
-	@return: true or false
- */
-bool LexAnalyzer::isAColc (char ch){
-	return (ch == '[');
-}//end isAColc
-
-
-
-/*
-	metodo que verifica se char e' fecha colchete (])
-	@return: true or false
- */
-bool LexAnalyzer::isFColc (char ch){
-	return (ch == ']');
-}//end isFColc
-
-
-
-/*
-	metodo que verifica se char e' apostrofo (')
-	@return: true or false
- */
-bool LexAnalyzer::isApost (char ch){
-	return (ch == '\'');
-}//end isApost
-
-
-
-/*
-	metodo que verifica se char e' sinal de exclamacao (!)
-	@return: true or false
- */
-bool LexAnalyzer::isExclam (char ch){
-	return (ch == '!');
-}//end isExclam
-
-
-
-/*
-	metodo que verifica se char e' sinal de interrogacao (?)
-	@return: true or false
- */
-bool LexAnalyzer::isQuestion (char ch){
-	return (ch == '?');
-}//end isQuestion
--- LexAnalyzer.h
+++ LexAnalyzer.h
@@ -26,31 +26,9 @@
 			void proxToken ();	
 			int getEstado ();
 			void setEstado (int est);
-			bool isIgual (char ch);
-			bool isAParent (char ch);
-			bool isFParent (char ch);
-			bool isMaior (char ch);
-			bool isMenor (char ch);
-			bool isVirgula (char ch);
-			bool isMais (char ch);
-			bool isMenos (char ch);
-			bool isMult (char ch);
-			bool isBarra (char ch);
-			bool isACha (char ch);
-			bool isFCha (char ch);
-			bool isAAspas (char ch);
-			bool isFAspas (char ch);
 			bool isNumero (char ch);
-			bool isSemicolon (char ch);
 			bool isLetraMi (char ch);
 			bool isLetraMa (char ch);
-			bool isSublinhado (char ch);
-			bool isDoisPonto (char ch);
-			bool isAColc (char ch);
-			bool isFColc (char ch);
-			bool isApost (char ch);
-			bool isExclam (char ch);
-			bool isQuestion (char ch);
 	};//end class
 
 #endif
